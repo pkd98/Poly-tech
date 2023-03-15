0821287ad4f7fa3fbd20dccabd2783b1cde8f8e3
PKD <baum12345@naver.com>
2023-03-15 13:43:29 +0900

    Ch11 수정

--- Java/JavaCh11/src/Exam/PoisonKinoko.java
+++ Java/JavaCh11/src/Exam/PoisonKinoko.java
@@ -1,21 +1,22 @@
 package Exam;
 
 public class PoisonKinoko extends Kinoko{
-    private int cnt = 5;
+    private int poisonCount;
     
     public PoisonKinoko(char suffix) {
         super(suffix);
+        poisonCount = 5;
     }
     
     @Override
     void attack(Hero hero) {
         super.attack(hero);
-        if(cnt != 0) {
+        if(poisonCount != 0) {
             System.out.println("추가로, 독 포자를 살포했다!");
             int damage = (int) (hero.getHp() * 0.2);
             hero.setHp(hero.getHp() - damage);
             System.out.println(damage + "포인트의 데미지");
-            cnt--;
+            poisonCount--;
         } else {
             System.out.println("독을 쓰진 못했다.");
         }
