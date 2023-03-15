3db2f46b72b408b686b9780983cd0daed7b87417
PKD <baum12345@naver.com>
2023-03-15 15:21:11 +0900

    ch12-1

---
+++ Java/JavaCh12/src/Dancer.java
@@ -0,0 +1,13 @@
+
+public class Dancer extends Character implements Human, Creature {
+    
+    public Dancer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void attack(Kinoko kinoko) {
+        // TODO Auto-generated method stub
+        
+    }
+}
