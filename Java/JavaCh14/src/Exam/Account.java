88cc8afb46e79c7ddccaf017bcefc5e71c9b4642
PKD <baum12345@naver.com>
2023-03-16 19:00:12 +0900

    Ch14

---
+++ Java/JavaCh14/src/Exam/Account.java
@@ -0,0 +1,38 @@
+package Exam;
+
+import java.util.Objects;
+
+public class Account {
+    private String accountNumber;
+    private int balance;
+    
+    public Account(String accountNumber, int balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return balance + "원 " + "(계좌번호=" + accountNumber + "))";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Account other = (Account) obj;
+        return Objects.equals(accountNumber.trim(), other.accountNumber.trim());
+    }
+    
+    
+
+}
---
+++ Java/JavaCh14/src/Exam/Main2.java
@@ -0,0 +1,13 @@
+package Exam;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        
+        Account a = new Account("4649", 1592);
+        Account b = new Account(" 4649", 1592);
+        System.out.println(a);
+        System.out.println(a.equals(b));
+        
+    }
+}
