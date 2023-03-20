b458f36c05e17728eac39afaf973be26a0a1a6b3
PKD <baum12345@naver.com>
2023-03-20 16:16:59 +0900

    Ch15-3

--- Java/JavaCh15/src/com/pkd/Exam/Main.java
+++ Java/JavaCh15/src/com/pkd/Exam/Main.java
@@ -3,14 +3,7 @@ package com.pkd.Exam;
 public class Main {
 
     public static void main(String[] args) {
-        String s = null;
-        try {
-            System.out.println(s.length());
-        } catch(NullPointerException e){
-            System.out.println("NullPointException 예외를 catch 하였습니다");
-            System.out.println("---- stack trace (여기부터) ----");
-            e.printStackTrace();
-            System.out.println("---- stack trace (여기까지) ----");
-        }
+        String s = "Tree";
+        int i = Integer.parseInt(s);
     }
 }
