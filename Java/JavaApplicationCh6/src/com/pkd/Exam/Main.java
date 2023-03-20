d3359b9d44e24bd8482f406f77bfd699ad2e2cbd
PKD <baum12345@naver.com>
2023-03-20 17:46:04 +0900

    응용Ch6-2

--- Java/JavaApplicationCh6/src/com/pkd/Exam/Main.java
+++ Java/JavaApplicationCh6/src/com/pkd/Exam/Main.java
@@ -1,20 +1,28 @@
 package com.pkd.Exam;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
+    // 6-1
+    public static String fileCopy(String fileName) throws IOException {
+        String newFileName = "";
 
-    public static void fileCopy(String file) throws IOException {
-        int index = file.lastIndexOf(".");
+        int index = fileName.lastIndexOf(".");
         StringBuilder sb = new StringBuilder();
-        sb.append(file.substring(0, index));
+        sb.append(fileName.substring(0, index));
         sb.append("(복사본)");
-        sb.append(file.substring(index));
-        
-        FileInputStream fis = new FileInputStream(file);
-        FileOutputStream fos = new FileOutputStream(sb.toString());
+        sb.append(fileName.substring(index));
+
+        newFileName = sb.toString();
+
+        FileInputStream fis = new FileInputStream(fileName);
+        FileOutputStream fos = new FileOutputStream(newFileName);
 
         int i = fis.read();
         while (i != -1) {
@@ -23,11 +31,28 @@ public class Main {
         }
         fis.close();
         fos.close();
+        return newFileName;
+    }
+
+    // 버퍼링을 통해 복사와 압축
+    public static void fileBufferingGzip(String fileName, String newFileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        BufferedOutputStream bos =
+                new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(newFileName)));
+
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            bos.write(line.getBytes());
+            bos.write("\n".getBytes());
+        }
+        br.close();
+        bos.close();
     }
 
     public static void main(String[] args) {
         try {
-            fileCopy("data.txt");
+            String fileName = "data.txt";
+            fileBufferingGzip(fileName, "data.zip");
         } catch (IOException e) {
             e.printStackTrace();
         }
